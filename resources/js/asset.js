4d35b72f989a9d182db142781a788644b9756236
Mark Walet <mark.walet@gmail.com>
2026-05-26 20:19:57 +0200

    Add the html block and ModalResponse::html() sugar
    Closes #25

--- resources/js/asset.js
+++ resources/js/asset.js
@@ -1,9 +1,11 @@
 import ModalActionResponse from './components/ModalActionResponse'
 import TextBlock from './components/TextBlock'
 import DividerBlock from './components/DividerBlock'
+import HtmlBlock from './components/HtmlBlock'
 
 Nova.booting(app => {
     app.component('modal-response', ModalActionResponse)
     app.component('modal-response-text-block', TextBlock)
     app.component('modal-response-divider-block', DividerBlock)
+    app.component('modal-response-html-block', HtmlBlock)
 });
