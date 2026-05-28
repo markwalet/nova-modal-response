39a948acfcd248f195a2178b675b23afdf2276f0
Mark Walet <mark.walet@gmail.com>
2026-05-28 10:08:03 +0200

    Demo action block in workbench

--- workbench/app/Nova/User.php
+++ workbench/app/Nova/User.php
@@ -17,6 +17,9 @@ use Laravel\Nova\Panel;
 use Laravel\Nova\ResourceTool;
 use Workbench\App\Nova\Actions\ViewBladeViewAction;
 use Workbench\App\Nova\Actions\ViewMarkdownAction;
+use Workbench\App\Nova\Actions\DemoChildModalAction;
+use Workbench\App\Nova\Actions\DemoToastAction;
+use Workbench\App\Nova\Actions\ViewActionBlockAction;
 use Workbench\App\Nova\Actions\ViewTextStackAction;
 use Workbench\App\Nova\Actions\ViewWithoutHighlightingAction;
 
@@ -116,6 +119,9 @@ class User extends Resource
             new ViewBladeViewAction,
             new ViewWithoutHighlightingAction,
             new ViewMarkdownAction,
+            new ViewActionBlockAction,
+            new DemoToastAction,
+            new DemoChildModalAction,
         ];
     }
 }
