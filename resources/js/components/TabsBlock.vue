c11db3ec3104c2b8798d19e888bc5675a4eafbb8
Mark Walet <mark.walet@gmail.com>
2026-05-28 10:17:17 +0200

    Make tabs full-width and flat instead of inset card

--- resources/js/components/TabsBlock.vue
+++ resources/js/components/TabsBlock.vue
@@ -1,36 +1,34 @@
 <template>
-    <div class="tab-group py-3 px-8">
-        <div class="tab-card">
-            <div
-                class="tab-menu divide-x dark:divide-gray-700 border-l-gray-200 border-r-gray-200 border-t-gray-200 border-b-gray-200 dark:border-l-gray-700 dark:border-r-gray-700 dark:border-t-gray-700 dark:border-b-gray-700"
-                role="tablist"
+    <div>
+        <div
+            class="flex border-b border-gray-200 dark:border-gray-700"
+            role="tablist"
+        >
+            <button
+                v-for="(tab, index) in tabs"
+                :key="index"
+                type="button"
+                class="px-6 py-3 text-sm focus:outline-none border-b-2"
+                :class="[
+                    index === activeIndex
+                        ? 'text-primary-500 font-bold border-primary-500'
+                        : 'text-gray-600 hover:text-gray-800 dark:text-gray-400 hover:dark:text-gray-200 border-transparent font-semibold',
+                ]"
+                role="tab"
+                :aria-selected="index === activeIndex"
+                @click.prevent="activeIndex = index"
             >
-                <button
-                    v-for="(tab, index) in tabs"
-                    :key="index"
-                    type="button"
-                    class="tab-item"
-                    :class="[
-                        index === activeIndex
-                            ? 'active text-primary-500 font-bold border-b-2 !border-b-primary-500'
-                            : 'text-gray-600 hover:text-gray-800 dark:text-gray-400 hover:dark:text-gray-200',
-                    ]"
-                    role="tab"
-                    :aria-selected="index === activeIndex"
-                    @click.prevent="activeIndex = index"
-                >
-                    <span class="capitalize">{{ tab.label }}</span>
-                </button>
-            </div>
+                <span class="capitalize">{{ tab.label }}</span>
+            </button>
+        </div>
 
-            <div v-if="activeTab" class="mt-2 -mx-8">
-                <component
-                    v-for="(child, childIndex) in (activeTab.value ?? [])"
-                    :is="blockComponents[child.type]"
-                    :key="childIndex"
-                    :block="child"
-                />
-            </div>
+        <div v-if="activeTab">
+            <component
+                v-for="(child, childIndex) in (activeTab.value ?? [])"
+                :is="blockComponents[child.type]"
+                :key="childIndex"
+                :block="child"
+            />
         </div>
     </div>
 </template>
